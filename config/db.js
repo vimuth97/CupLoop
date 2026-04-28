b84d1f21be29ba8e632e96894a6383fc44e5afda
VimuthR <vimuth.rm@gmail.com>
2026-04-28 21:18:13 +1000

    database connection

---
+++ config/db.js
@@ -0,0 +1,14 @@
+// db.js
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ server/README.md
@@ -0,0 +1,1 @@
+CupLoop Backend
